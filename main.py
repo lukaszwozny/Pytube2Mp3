e0ed48383d6a56f8f927d0342cdbc76070ec043c
DESKTOP-AC2POTG\Admin <tamburyniarz@gmail.com>
2017-09-12 21:49:25 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
+import os
+import ntpath
+
+import shutil
 from pytube import YouTube
+import moviepy.editor as mp
 
 
-def download_yt_video(url, path):
+def download_yt_video(url):
+    video_dir = 'tmp'
+    if not os.path.exists(video_dir):
+        os.makedirs(video_dir)
     yt = YouTube(url=url)
-    video = yt.filter('mp4')[0]
-    video.download(path)
+    extension = 'mp4'
+    vid_path = video_dir + '/' + yt.filename + '.' + extension
+    video = yt.filter(extension)[0]
+    video.download(video_dir)
+    return vid_path
+
+
+def convert_video_to_mp3(vid_path):
+    clip = mp.VideoFileClip(vid_path)
+    vid_filename = ntpath.basename(vid_path)
+    mp3_filename = ntpath.splitext(vid_filename)[0] + '.mp3'
+    clip.audio.write_audiofile(mp3_filename)
+    del clip.reader
+    del clip
+    # Move mp3 to mp3/
+    mp3_dir = 'mp3'
+    if not os.path.exists(mp3_dir):
+        os.makedirs(mp3_dir)
+    shutil.move(mp3_filename, mp3_dir + '/' + mp3_filename)
 
 
 def main():
-    url = 'https://www.youtube.com/watch?v=HgzGwKwLmgM'
-    path = 'tmp'
-    download_yt_video(url=url, path=path)
+    url = 'https://www.youtube.com/watch?v=DebhiaQH3ps'
+    vid_path = download_yt_video(url=url)
+    convert_video_to_mp3(vid_path)
+    os.remove(vid_path)
 
 
 if __name__ == "__main__":
